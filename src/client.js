1319cca2e28cb4972f278e448ddd6c558cd85848
chenjianhua <chenjianhua@gf.com.cn>
2018-05-02 11:07:00 +0800

    add webpack

---
+++ src/client.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: edvardchen
+ * @Date: 2018-05-02 10:57:45
+ * @Last Modified by: edvardchen
+ * @Last Modified time: 2018-05-02 10:58:20
+ */
+
+document.body.innerText = 'Hello World';
