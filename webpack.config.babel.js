907003754a4d6407a8c1aa6e317d1ff05d2bc2b1
chenjianhua <chenjianhua@gf.com.cn>
2018-05-02 16:48:37 +0800

    HMR

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,17 +1,26 @@
+import webpack from 'webpack';
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default (env, argv) => {
-  const DEV = !!argv.mode === 'development';
+  const DEV = argv.mode === 'development';
   const config = {
     entry: {
       app: './src/client.js'
     },
     devtool: DEV && 'eval',
-    plugins: [new HtmlWebpackPlugin()],
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new webpack.NamedModulesPlugin(),
+      DEV && new webpack.HotModuleReplacementPlugin()
+    ].filter(Boolean),
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, DEV ? '.tmp' : 'dist')
+      path: path.resolve(__dirname, 'dist')
     }
   };
   return config;
